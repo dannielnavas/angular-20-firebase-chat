3363c14e662cee209c93f707a6cc741ec4f96f52
Danniel Navas <dannielnavas@gmail.com>
2025-10-07 07:53:58 -0500

    feat: :sparkles: Define application routes for authentication and chat components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'auth',
+    pathMatch: 'full',
+  },
+  {
+    path: 'auth',
+    loadComponent: () => import('./components/auth/auth').then((c) => c.Auth),
+    title: 'Authentication - asistente',
+  },
+  {
+    path: 'chat',
+    loadComponent: () => import('./components/chat/chat').then((c) => c.Chat),
+    title: 'Chat - asistente',
+  },
+  {
+    path: '**',
+    redirectTo: 'auth',
+  },
+];
