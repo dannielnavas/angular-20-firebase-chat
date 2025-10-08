5794c6fe0550692d2d791da14c26ee1405744054
Danniel Navas <dannielnavas@gmail.com>
2025-10-08 07:34:11 -0500

    feat: :sparkles: Refactor app configuration to include HTTP client and Firebase services

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,33 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import { provideHttpClient } from '@angular/common/http';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { provideAuth } from '@angular/fire/auth';
+import { provideFirestore } from '@angular/fire/firestore';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { environment } from '../environments/environment';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideHttpClient(),
+    provideFirebaseApp(() => {
+      return initializeApp(environment.firebaseConfig);
+    }),
+    provideAuth(() => {
+      return getAuth();
+    }),
+    provideFirestore(() => {
+      return getFirestore();
+    }),
+  ],
 };
